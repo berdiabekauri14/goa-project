d265623727b0c116eb6ea2f0ce6ffe74a112b16c
Berdia <berdia@Berdias-MacBook-Pro.local>
2025-10-01 18:31:06 +0400

    commit

--- server/controller/comments.controller.js
+++ server/controller/comments.controller.js
@@ -1,4 +1,8 @@
-const comments = require("../data/comments..data.js")
+const comments = [
+    { id: 1, comment: "This acadamy is very good! great job!"},
+    { id: 2, comment: "I want to learn too!" },
+    { id: 3, comment: "The best academy in georgia!" }
+]
 
 const getComments = (req, res) => {
     res.json(comments)
